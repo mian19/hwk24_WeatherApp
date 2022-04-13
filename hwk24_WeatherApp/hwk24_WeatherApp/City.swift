b6e4e299838b75efa99955cb6b1d8a3819f81a96
Anton Mishkevich <mian19@protonmail.com>
2022-04-13 20:53:39 +0300

    feat: add main functions for work with JSON

---
+++ hwk24_WeatherApp/hwk24_WeatherApp/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  hwk24_WeatherApp
+//
+//  Created by Kyzu on 13.04.22.
+//
+
+import Foundation
+
+struct City: Hashable {
+    let name: String
+    let latitude: Double
+    let longetude: Double
+    let country: String
+    let state: String?
+}
